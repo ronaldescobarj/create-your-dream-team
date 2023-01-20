2337d7a3c112c11d848f8447f7b82755f6462f86
Ronald Escobar <ronaldescobarj@gmail.com>
2023-01-19 23:00:31 -0400

    added layout wrapper with header, footer and children. Moved header data into app and document. Wrapped hello world in layout.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import LayoutWrapper from "@/components/LayoutWrapper";
+import React from "react";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <LayoutWrapper>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    </LayoutWrapper>
+  );
 }
