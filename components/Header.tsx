60618fe6b9af0840117610faaca1df14092ae31a
Ronald Escobar <ronaldescobarj@gmail.com>
2023-02-04 16:36:39 -0400

    added about and create-team components and wrapped them with layout. Added links to navigate.

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,14 @@ import React from "react";
 export default function Header() {
   return (
     <header className="bg-slate-50 flex items-center justify-between py-5 pl-5 pr-10">
-      <h1 className="text-4xl text-lime-900">
-        build your <b className="bg-lime-900 text-slate-50">Dream Team</b>
-      </h1>
-      <Link href='/about' className="text-lime-900">About</Link>
+      <Link href="/">
+        <h1 className="text-4xl text-lime-900">
+          build your <b className="bg-lime-900 text-slate-50">Dream Team</b>
+        </h1>
+      </Link>
+      <Link href="/about" className="text-lime-900">
+        About
+      </Link>
     </header>
   );
 }
---
+++ pages/create-team.tsx
@@ -0,0 +1,10 @@
+import LayoutWrapper from '@/components/LayoutWrapper'
+import React from 'react'
+
+export default function CreateTeam() {
+  return (
+    <LayoutWrapper>
+      <p>create-team</p>
+    </LayoutWrapper>
+  );
+}
